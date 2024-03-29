0c34d8353e52e8b0af6dd6dc8edc8a6cc139e2a6
Omnia254 <76522001+Omnia254@users.noreply.github.com>
2024-03-29 20:26:16 +0200

    Example and presentation

---
+++ Flyweight Examples/flyweight/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            game game = new game();
+            game.addparticles("red", 10);
+            game.addparticles("red", 20);
+            game.display();
+        }
+    }
+    class particle
+    {
+        public string color { get; set; }
+
+        public particle(string _color)
+        {
+            color = _color;
+        }
+
+        public void display(int speed)
+        {
+            Console.WriteLine("Element: " + color + " ,speed: " + speed);
+        }
+    }
+
+    class movingparticles
+    {
+        particle partical;
+        public int speed { get; set; } 
+        public movingparticles(string _color,int _speed)
+        {
+            partical = factory.getpartical(_color);
+            speed = _speed;
+        }
+
+        public void display()
+        {
+          partical.display(speed);
+           
+        }
+    }
+    class factory
+    {
+        static Dictionary<string, particle> particles = 
+            new Dictionary<string, particle>();
+        public static particle getpartical(string c)
+        {
+            if (particles.ContainsKey(c))
+            {
+                return particles[c];
+            }
+            else
+            {
+                return particles[c] = new particle(c);
+            }
+
+        }
+    }
+
+    class game
+    {
+        List<movingparticles> movingparticles = new List<movingparticles>();
+        public void addparticles(string color,int speed)
+        {
+            movingparticles.Add(new movingparticles(color, speed));
+        }
+        public void display()
+        {
+            foreach(var item in movingparticles)
+            {
+                item.display();
+            }
+        }
+    }
+}
+
